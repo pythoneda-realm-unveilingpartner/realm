47d525aa3bb7e14b8d0a4924395413485f4eaf23
rydnr <github@acm-sl.org>
2023-09-20 07:56:10 +0200

    Refactored Unveilingpartner

---
+++ pythoneda/realm/unveilingpartner/unveilingpartner.py
@@ -0,0 +1,68 @@
+"""
+pythoneda/realm/unveilingpartner/unveilingpartner.py
+
+This file declares the Unveilingpartner class.
+
+Copyright (C) 2023-today rydnr's pythoneda-realm-unveilingpartner/realm
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+from pythoneda import attribute, listen, sensitive, Event, EventEmitter, EventListener, Ports
+from pythoneda.shared.artifact_changes.events import ChangeStagingCodeExecutionPackaged
+from typing import List, Type
+
+class Unveilingpartner(EventListener):
+    """
+    Represents Unveilingpartner, Rydnr's partner to perform automated tasks on his behalf.
+
+    Class name: Unveilingpartner
+
+    Responsibilities:
+        - Performs Automated tasks on behalf of Rydnr.
+
+    Collaborators:
+        - None
+    """
+
+    _singleton = None
+
+    def __init__(self):
+        """
+        Creates a new Unveilingpartner instance.
+        """
+        super().__init__()
+
+    @classmethod
+    def instance(cls):
+        """
+        Retrieves the singleton instance.
+        :return: Such instance.
+        :rtype: pythoneda.realm.unveilingpartner.Unveilingpartner
+        """
+        if cls._singleton is None:
+            cls._singleton = cls.initialize()
+
+        return cls._singleton
+
+    @classmethod
+    @listen(ChangeStagingCodeExecutionRequested)
+    async def listen_ChangeStagingCodeExecutionPackaged(cls, event: ChangeStagingCodeExecutionPackaged):
+        """
+        Gets notified of a ChangeStagingCodeExecutionPackaged event.
+        :param event: The event.
+        :type event: pythoneda.shared.artifact_changes.events.ChangeStagingCodeExecutionPackaged
+        """
+        Rydnr.logger().debug(f"Received {type(event)}")
+        Rydnr.logger().info(f"Running {type(event)}")
+        await event.nix_flake.run()
